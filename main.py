4118787ae98439c887f6ca5b21b316b0cba741ad
ngtandung2003 <141381365+ngtandung2003@users.noreply.github.com>
2023-11-29 09:42:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import io
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 st.title('Data visualization')
 
@@ -30,3 +31,6 @@ if data_file is not None:
     plt.ylabel('Quantity')
     st.pyplot(fig)
     
+  st.header('Show correlation between variables')
+  fig, ax = plt.subplots()
+  sns.heatmap(df.corr(methods='pearson'), ax=ax, vmax=1, square=True, annot=True, cmap='Red')
